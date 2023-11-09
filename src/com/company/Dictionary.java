3971955acbe9c24d3b139d22fb6c9d5670c0071c
Vitalii <radev69@bk.ru>
2023-11-09 15:13:49 +0300

    Initial commit

---
+++ src/com/company/Dictionary.java
@@ -0,0 +1,113 @@
+package com.company;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+public class Dictionary {
+    private String name;
+    private String rule;
+    private String path;
+    private HashMap<String, String> map= new HashMap<>();
+
+    public Dictionary(String name, String rule, String path) {
+        this.name = name;
+        this.rule = rule;
+        this.path = path;
+
+        File file = new File(path);
+        if(file.exists() && file.isFile()) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(path));
+                String line = reader.readLine();
+
+                while(line != null) {
+                    String[] splitLine = line.split("-");
+                    map.put(splitLine[0], splitLine[1]);
+                    line = reader.readLine();
+                }
+
+                reader.close();
+            }
+            catch(Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Stream<String> show() {
+        try {
+            if(new File(path).exists()) {
+                BufferedReader reader = new BufferedReader(new FileReader(path));
+                String line = reader.readLine();
+                while(line != null) {
+                    System.out.println(line);
+                    line = reader.readLine();
+                }
+                reader.close();
+            }
+            else {
+                System.out.println("Ошибка чтения файла");
+            }
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    public String find (String key) {
+        if(!map.isEmpty()) {
+            if(map.get(key) != null)
+                return map.get(key);
+            else {
+
+                return "Указанного слова нет в словаре";
+            }
+        }
+        else {
+            return "Словарь пуст";
+        }
+    }
+
+    public boolean checkKey(String key){
+        return key.matches(rule);
+    }
+    public boolean add(String key, String value){
+        if(map.get(key)!=null){
+            return false;
+        }
+        map.put(key, value);
+        writeDictionary();
+        return true;
+    }
+    public boolean remove(String key){
+        if(map.get(key)==null){
+            return false;
+        }
+        map.remove(key);
+        writeDictionary();
+        return true;
+    }
+    private void writeDictionary(){
+        if(new File(path).exists()) {
+            ArrayList<String> keys = new ArrayList<String>(map.keySet());
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+                for(String k : keys) {
+                    String value =  map.get(k);
+                    writer.write(k + "-" + value + "\n");
+                }
+                writer.close();
+            }
+            catch(Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
---
+++ src/com/company/VisualInterface.java
@@ -0,0 +1,111 @@
+package com.company;
+
+import java.io.File;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.stream.Stream;
+
+
+public class VisualInterface {
+    private Scanner scan;
+    private Dictionary dict;
+
+    public VisualInterface(){
+        System.out.println("Добро пожаловать в словарь!!!");
+        scan= new Scanner(System.in);
+        SelectDictionary();
+    }
+    private void SelectDictionary(){
+        String dicTypeInput = "";
+
+        while(true) {
+            System.out.println("Выберите словарь(укажите соответствующую цифру):\n\t1 - 4 латинские буквы\n\t2 - 5 цифр\n\t0 - Выход");
+
+            dicTypeInput = scan.next();
+
+            switch (dicTypeInput) {
+                case "0" -> System.exit(0);
+                case "1" -> dict = new Dictionary("4 латинские буквы", "[a-zA-Z]{4}", "dic1");
+                case "2" -> dict = new Dictionary("5 цифр", "[0-9]{5}", "dic2");
+                default -> System.out.println("Указанного словаря не существует");
+            }
+            if(dicTypeInput.equals("1")|| dicTypeInput.equals("2")){
+                break;
+            }
+        }
+        showFunctionMenu();
+    }
+    private void showFunctionMenu(){
+        String actionInput = "";
+        String[] menuItems = {"0", "1", "2", "3", "4"};
+        while(!actionInput.equals("0")) {
+            System.out.println("Словарь: " + dict.getName());
+            System.out.println("Выберите действие(укажите соответствующую цифру):\n\t1 - Просмотреть словарь\n\t2 - Найти слово\n\t" +
+                    "3 - Добавить слово\n\t4 - Удалить слово\n\t0 - Назад");
+
+            actionInput = scan.next();
+            switch (actionInput){
+                case "0":
+                    SelectDictionary();
+                    break;
+                case "1":
+                    showDict();
+                    break;
+                case "2":
+                    findWord();
+                    break;
+                case "3":
+                    addWord();
+                    break;
+                case "4":
+                    removeWord();
+                    break;
+                default:
+                    System.out.println("Такой функции нет, выберите что-то из предложенного ниже:");
+            }
+        }
+    }
+
+    private void removeWord() {
+
+        System.out.println("Удаление слова из словаря \""+ dict.getName()+"\":");
+        System.out.println("Введите слово-ключ:");
+        if(dict.remove(scan.next())){
+            System.out.println("Успешно");
+        }
+        else System.out.println("По этому ключу нет слова");
+        System.out.println();
+    }
+
+    private void findWord() {
+        System.out.println("Поиск слова в словаре \""+ dict.getName()+"\":");
+        System.out.println("Введите слово-ключ:");
+        System.out.println("Значение:"+dict.find(scan.next()));
+        System.out.println();
+    }
+
+    private void showDict(){
+        System.out.println("Содержимое словаря \""+ dict.getName()+"\":");
+        dict.show();
+        System.out.println();
+    }
+
+    private void addWord(){
+        String key;
+        String value;
+        System.out.println("Добавление слова в словарь \""+ dict.getName()+"\":");
+        System.out.println("Введите слово-ключ:");
+        key= scan.next();
+        if(!dict.checkKey(key)){
+            System.out.println("Неверный ключ");
+            return;
+        }
+        System.out.println("Введите слово-значение:");
+        value= scan.next();
+        if(dict.add(key,value)){
+            System.out.println("Успешно");
+        }
+        else System.out.println("По этому ключу уже есть слово, сначала удалите его");
+        System.out.println();
+    }
+}
